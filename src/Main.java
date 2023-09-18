39be856d5a7c9651367e6f43a151bcda25eb2d9f
Geka58 <he588@yandex.ru>
2023-09-18 15:26:44 +0300

    main12

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        //main 11
+        //main
         System.out.println("Hello world!");
         System.out.println("Hello world!");
     }
