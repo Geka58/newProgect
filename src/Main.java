1e05254528072bea2993885451953208038dd96c
Geka58 <he588@yandex.ru>
2023-09-19 12:17:59 +0300

    test1

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        //hello test6
+        //hello test1
         System.out.println("Hello world!!!!!!!!!!");
-        System.out.println("Hello world!!!!!!!!!!");
-        System.out.println("hello!!!!!!!!!!!!!!!!");
-
-        //main test
-        System.out.println("Hello world!!!!");
-        System.out.println("Hello world!!!!");
- 
     }
 }
